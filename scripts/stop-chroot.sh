8c6e5148ec1abeb52f10b3b95c33457ad513427c
Farrell Rafi <farrellabdurraafi@yahoo.com>
2016-05-21 00:02:43 -0700

    Update stop-chroot.sh

--- scripts/stop-chroot.sh
+++ scripts/stop-chroot.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export dir=/var/chroot/
-lsof | grep " /chroot/" > $dir/process/1
+lsof | grep " $dir/environment/" > $dir/process/1
 cut -c11-15 $dir/process/1 > $dir/process/2
 uniq $dir/process/2 > $dir/process/3
 echo "
@@ -9,8 +9,8 @@ sed 's/ //g' $dir/process/4 > $dir/process/5
 sed ':a;N;$!ba;s/\n/\nkill -9 /g' $dir/process/5 > $dir/process/6
 sed '1 d' $dir/process/6 > $dir/process/7
 sh $dir/process/6
-umount /chroot/sys
-umount /chroot/proc
-umount /chroot/dev
-umount /chroot/etc/resolv.conf
-umount /chroot
+umount $dir/environment/sys
+umount $dir/environment/proc
+umount $dir/environment/dev
+umount $dir/environment/etc/resolv.conf
+umount $dir/environment
