e1d016874f0277bd3d30b6017aab6415740f095f
Farrell Rafi <farrellabdurraafi@yahoo.com>
2016-05-21 00:03:44 -0700

    Update start-chroot.sh

--- scripts/start-chroot.sh
+++ scripts/start-chroot.sh
@@ -4,9 +4,9 @@ echo "Your chroot images:
 $(ls $dir/images/ | sed 's/.img//g')
 "
 read -p "Type your image name: " image
-mount $dir/images/$image.img $dir/chroot/environment
-mount -o bind /etc/resolv.conf $dir/chroot/environment/etc/resolv.conf
-mount -o bind /dev $dir/chroot/environment/dev
-mount -o bind /proc $dir/chroot/environment/proc
-mount -o bind /sys $dir/chroot/environment/sys
-chroot $dir/environment/chroot /bin/su -c 'sh /boot/boot.sh'
+mount $dir/images/$image.img $dir/environment
+mount -o bind /etc/resolv.conf $dir/environment/etc/resolv.conf
+mount -o bind /dev $dir/environment/dev
+mount -o bind /proc $dir/environment/proc
+mount -o bind /sys $dir/environment/sys
+chroot $dir/environment /bin/su -c 'sh /boot/boot.sh'
