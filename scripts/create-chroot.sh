96ded11f390daa159f5e9700abdc959666a70162
Farrell Rafi <farrellabdurraafi@yahoo.com>
2016-05-20 23:56:18 -0700

    Update create-chroot.sh

--- scripts/create-chroot.sh
+++ scripts/create-chroot.sh
@@ -4,11 +4,12 @@ read -p "Type your new chroot name: " chroot
 read -p "Type your new chroot size: " size
 dd if=/dev/zero of=$dir/images/$chroot.img bs=1 count=0 seek=$size
 mke2fs -t ext4 $dir/images/$chroot.img
-mount $dir/images/$chroot.img /chroot
-rm -rf /chroot/*
+mount $dir/images/$chroot.img $dir/chroot/environment
+rm -rf $dir/chroot/environment/*
+read -p "Type your architecture: " arch
 read -p "Type your distro codename: " distro
 read -p "Type your mirror url (leave blank for default mirror): " mirror
-debootstrap --arch i386 $distro /chroot/ $mirror &&
-chroot /chroot /bin/su -c 'echo "#!/bin/sh" > /boot/boot.sh' &&
-chroot /chroot /bin/su -c 'echo "" > /etc/resolv.conf'
-umount /chroot
+debootstrap --arch $arch $distro $dir/chroot/environment/ $mirror &&
+chroot $dir/chroot/environment /bin/su -c 'echo "#!/bin/sh" > /boot/boot.sh' &&
+chroot $dir/chroot/environment /bin/su -c 'echo "" > /etc/resolv.conf'
+umount $dir/chroot/environment
