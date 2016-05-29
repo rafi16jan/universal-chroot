b28a25ac5d15b56c3cc04074bc961d39f2803ee5
Farrell Rafi <farrellabdurraafi@yahoo.com>
2016-05-29 15:56:12 +0700

    Update create-chroot.sh

--- scripts/create-chroot.sh
+++ scripts/create-chroot.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 export dir=/var/chroot/
 read -p "Type your new chroot name: " chroot
-read -p "Type your new chroot size: " size
-dd if=/dev/zero of=$dir/images/$chroot.img bs=1 count=0 seek=$size
+read -p "Type your new chroot size (in GB): " size
+dd if=/dev/zero of=$dir/images/$chroot.img bs=1G count=$size
 mke2fs -t ext4 $dir/images/$chroot.img
 mount $dir/images/$chroot.img $dir/environment
 rm -rf $dir/environment/*
